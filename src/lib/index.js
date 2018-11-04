e6b252f921ca9892ae753acbf1a487ce9cfe7b87
adamkvasnicka <kvasnad1@uhk.cz>
2018-11-04 17:50:44 +0100

    Upgraded version

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,45 +1,42 @@
-import './atoms/Grid/Grid';
-import './atoms/Grid/GridCol';
-import './atoms/Grid/GridTile';
-import './atoms/Checkbox';
-import './atoms/Divider';
-import './atoms/Heading';
-import './atoms/List/List';
-import './atoms/List/ListItem';
-import './atoms/List/ListItemText';
-import './atoms/Media';
-import './atoms/Overlay/Overlay';
-import './atoms/Overlay/OverlayHeading';
-import './atoms/Paper';
-import './atoms/Paragraph';
-import './atoms/RadioButton';
-import './atoms/Spacer';
-import './atoms/Table/Table';
-import './atoms/Table/TableCell';
-import './atoms/Table/TableRow';
-import './atoms/Table/SimpleTableRow';
-
-import './molecules/BottomNavigation';
-import './molecules/Dialog';
-import './molecules/ExpansionPanel';
-import './molecules/NativeSelectField';
-import './molecules/Notification';
-import './molecules/ResponsiveDrawer';
-import './molecules/SearchField';
-import './molecules/SelectField';
-import './molecules/Stepper';
-import './molecules/Tab';
-import './molecules/TextField';
-
-import './organisms/ActionList';
-import './organisms/Autocomplete';
-import './organisms/Calendar';
-import './organisms/GriddedTextField';
-import './organisms/SortTable';
-import './organisms/TreeTable';
-
-import './transition/Bounce';
-import './transition/Fade';
-import './transition/Slide';
-
+/* eslint-disable import/named */
 export { default as Button } from './atoms/Button/Button';
+export { default as RoundButton } from './atoms/Button/RoundButton';
+export { default as Grid } from './atoms/Grid/Grid';
+export { default as GridCol } from './atoms/Grid/GridCol';
+export { default as GridTile } from './atoms/Grid/GridTile';
+export { default as Checkbox } from './atoms/Checkbox';
+export { default as Divider } from './atoms/Divider';
+export { default as Heading } from './atoms/Heading';
+export { default as List } from './atoms/List/List';
+export { default as ListItem } from './atoms/List/ListItem';
+export { default as ListItemText } from './atoms/List/ListItemText';
+export { default as Media } from './atoms/Media';
+export { default as Overlay } from './atoms/Overlay/Overlay';
+export { default as OverlayHeading } from './atoms/Overlay/OverlayHeading';
+export { default as Paragraph } from './atoms/Paragraph';
+export { default as RadioButton } from './atoms/RadioButton';
+export { default as Spacer } from './atoms/Spacer';
+export { default as Paper } from './atoms/Paper';
+export { default as Table } from './atoms/Table/Table';
+export { default as TableCell } from './atoms/Table/TableCell';
+export { default as TableRow } from './atoms/Table/TableRow';
+export { default as SimpleTableRow } from './atoms/Table/SimpleTableRow';
+
+export { default as BottomNavigation } from './molecules/BottomNavigation';
+export { default as Dialog } from './molecules/Dialog';
+export { default as ExpansionPanel } from './molecules/ExpansionPanel';
+export { default as NativeSelectField } from './molecules/NativeSelectField';
+export { default as Notification } from './molecules/Notification';
+export { default as ResponsiveDrawer } from './molecules/ResponsiveDrawer';
+export { default as SearchField } from './molecules/SearchField';
+export { default as SelectField } from './molecules/SelectField';
+export { default as Stepper } from './molecules/Stepper';
+export { default as Tab } from './molecules/Tab';
+export { default as TextField } from './molecules/TextField';
+
+export { default as ActionList } from './organisms/ActionList';
+export { default as Autocomplete } from './organisms/Autocomplete';
+export { default as Calendar } from './organisms/Calendar';
+export { default as GriddedTextField } from './organisms/GriddedTextField';
+export { default as SortTable } from './organisms/SortTable';
+export { default as TreeTable } from './organisms/TreeTable';
